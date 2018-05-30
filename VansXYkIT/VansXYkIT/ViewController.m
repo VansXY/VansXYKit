5355bf4fc0fdca4195e127c906da978fb6b2f5c9
肖扬 <xy_dtb@163.com>
2018-05-30 16:58:24 +0800

    上传文件

--- VansXYkIT/VansXYkIT/ViewController.m
+++ VansXYkIT/VansXYkIT/ViewController.m
@@ -18,6 +18,12 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"develop");
+    [self addMethod];
+    
+}
+
+- (void)addMethod {
+    NSLog(@"addMethod");
 }
 
 
